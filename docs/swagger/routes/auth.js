3a2d5b3b551ecf9d33818829df023088d8d3c90c
yotech1324 <105988202+yotech1324@users.noreply.github.com>
2025-01-15 19:37:16 +0530

    Add files via upload
    first commit

---
+++ docs/swagger/routes/auth.js
@@ -0,0 +1,167 @@
+module.exports = {
+  '/auth/sign-up': {
+    post: {
+      tags: ['Auth'],
+      summary: 'Create New Account',
+      requestBody: {
+        required: true,
+        content: {
+          'application/x-www-form-urlencoded': {
+            schema: {
+              type: 'object',
+              properties: {
+                fullName: {
+                  type: 'string',
+                },
+                email: {
+                  type: 'string',
+                },
+                newPassword: {
+                  type: 'string',
+                },
+                confirmNewPassword: {
+                  type: 'string',
+                },
+                phone: {
+                  type: 'string',
+                },
+                Role: {
+                  type: 'string',
+                },
+              },
+              required: [
+                'fullName',
+                'email',
+                'newPassword',
+                'confirmNewPassword',
+                'phone',
+                'Role',
+              ],
+            },
+          },
+        },
+      },
+      responses: {
+        201: {
+          description: 'Create New Account',
+        },
+      },
+    },
+  },
+  '/auth/sign-in': {
+    post: {
+      tags: ['Auth'],
+      summary: 'Login Your Account',
+      requestBody: {
+        required: true,
+        content: {
+          'application/x-www-form-urlencoded': {
+            schema: {
+              type: 'object',
+              properties: {
+                email: {
+                  type: 'string',
+                },
+                password: {
+                  type: 'string',
+                },
+              },
+              required: ['email', 'password'],
+            },
+          },
+        },
+      },
+      responses: {
+        200: {
+          description: 'Login Your Account',
+        },
+      },
+    },
+  },
+  '/auth/refresh-token': {
+    post: {
+      tags: ['Auth'],
+      summary: 'Get Access Token',
+      produces: ['application/json'],
+      security: [
+        {
+          auth_token: [],
+        },
+      ],
+      requestBody: {
+        required: true,
+        content: {
+          'application/x-www-form-urlencoded': {
+            schema: {
+              type: 'object',
+              properties: {
+                email: {
+                  type: 'string',
+                },
+                refreshToken: {
+                  type: 'string',
+                },
+              },
+              required: ['email', 'refreshToken'],
+            },
+          },
+        },
+      },
+      responses: {
+        200: {
+          description: 'Get Access Token from Refresh Token',
+        },
+      },
+    },
+  },
+  '/logout': {
+    post: {
+      tags: ['Auth'],
+      summary: 'Logout',
+      produces: ['application/json'],
+      security: [
+        {
+          auth_token: [],
+        },
+      ],
+      requestBody: {
+        required: true,
+        content: {
+          'application/x-www-form-urlencoded': {
+            schema: {
+              type: 'object',
+              properties: {
+                UserId: {
+                  type: 'string',
+                },
+              },
+              required: ['UserId'],
+            },
+          },
+        },
+      },
+      responses: {
+        200: {
+          description: 'Terminate your api access',
+        },
+      },
+    },
+  },
+  '/profile': {
+    get: {
+      tags: ['Auth'],
+      summary: 'Get Profile',
+      produces: ['application/json'],
+      security: [
+        {
+          auth_token: [],
+        },
+      ],
+      responses: {
+        200: {
+          description: 'Get Profile',
+        },
+      },
+    },
+  },
+}
